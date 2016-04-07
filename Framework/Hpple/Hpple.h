39b388430de0f279799caa1177bbaee99902d260
Mathias Köhnke <mathias.koehnke@web.de>
2016-04-07 16:52:59 +0200

    Added Public Headers

--- Framework/Hpple/Hpple.h
+++ Framework/Hpple/Hpple.h
@@ -37,4 +37,5 @@ FOUNDATION_EXPORT const unsigned char HppleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Hpple/PublicHeader.h>
 
-
+#import <Hpple/TFHpple.h>
+#import <Hpple/TFHppleElement.h>
