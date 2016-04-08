31e334f56953d8ff38c4d36910ea364d4f465fa6
Mathias Köhnke <mathias.koehnke@web.de>
2016-04-08 16:06:15 +0200

    Replaced UIKit import with Foundation import

--- Framework/Hpple/Hpple.h
+++ Framework/Hpple/Hpple.h
@@ -27,7 +27,7 @@
 //  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Hpple.
 FOUNDATION_EXPORT double HppleVersionNumber;
