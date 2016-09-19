435930f625605655f16ad6e9f4c3198d19c211a8
Mathias Köhnke <github@mathiaskoehnke.de>
2016-09-19 21:23:53 +0200

    Removed redundant dependency

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@ import PackageDescription
 
 let package = Package(
   name: "hpple",
-  targets: [],
-  dependencies: [
-        .Package(url: "https://github.com/mkoehnke/Clibxml2.git", majorVersion: 1)
-  ]
+  targets: []
+  //dependencies: [
+  //      .Package(url: "https://github.com/mkoehnke/Clibxml2.git", majorVersion: 1)
+  //]
 )
